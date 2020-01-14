14d0c50d8d390239011ea254bc31e1bda36a672d
songtianyi917 <59803128+celiasty917@users.noreply.github.com>
2020-01-14 20:18:29 +0800

    Updates

---
+++ precache-manifest.78b3448b39d85e1cbe6765d7dc3076fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0a4ebd450596110794403063dc9910",
+    "url": "/get_github_user_info/my-app/index.html"
+  },
+  {
+    "revision": "f508f357fe615b24b2ad",
+    "url": "/get_github_user_info/my-app/static/css/main.ec219aea.chunk.css"
+  },
+  {
+    "revision": "fe541516ad74cb061866",
+    "url": "/get_github_user_info/my-app/static/js/2.32bd14a7.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/get_github_user_info/my-app/static/js/2.32bd14a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f508f357fe615b24b2ad",
+    "url": "/get_github_user_info/my-app/static/js/main.99afd42f.chunk.js"
+  },
+  {
+    "revision": "cd87ddadbd22f5e97f9c",
+    "url": "/get_github_user_info/my-app/static/js/runtime-main.4d3eb983.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/get_github_user_info/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
